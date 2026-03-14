d2028d5f9953fa9bf2ea51cb6ad92ebe610e4898
MarkDemidovs <markdemidovs@proton.me>
2026-03-14 21:46:55 +0200

    feat: routes endpoints.ts

--- server/src/routes/endpoints.ts
+++ server/src/routes/endpoints.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { getEndpoints, createEndpoint } from "../controllers/endpoints";
+import { getEndpoints, createEndpoint, deleteEndpoint, getRequests, deleteRequests } from "../controllers/endpoints";
 import { requireAuth } from "../middleware/auth";
+import { get } from "node:http";
 
 const router = Router();
 
 router.get("/", requireAuth, getEndpoints);
 router.post("/", requireAuth, createEndpoint);
+router.delete("/:id", requireAuth, deleteEndpoint);
+router.get("/:id/requests", requireAuth, getRequests);
+router.delete("/:id/requests", requireAuth, deleteRequests);
 
 export default router;
