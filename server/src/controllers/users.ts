8a6296cad16ad8377115bbdc4ad97c8aaa3ae73a
MarkDemidovs <markdemidovs@proton.me>
2026-03-14 03:50:08 +0200

    feat: server side registtration

--- server/src/controllers/users.ts
+++ server/src/controllers/users.ts
@@ -5,8 +5,13 @@ import jwt from "jsonwebtoken";
 
 
 export const register = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
     try {
-        const { rows } = await pool.query("SELECT * FROM users");
+        const hashed = await bcrypt.hash(password, 10);
+        const { rows } = await pool.query(
+            "INSERT INTO users (email, password_hash) VALUES ($1, $2)", 
+            [email, hashed]
+        );
         res.status(201).json(rows[0]);
     } catch (err) {
         console.log(err);
