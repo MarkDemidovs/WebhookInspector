a504832365c00d5701ee9e519b4a634bebd71da2
MarkDemidovs <markdemidovs@proton.me>
2026-03-14 20:44:52 +0200

    feat: endpoints getEndpoints controller

---
+++ server/src/controllers/endpoints.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { pool } from "../db";
+
+export const getEndpoints = async (req: Request, res: Response) => {
+    const userId = (req as any).userId;
+    try {
+        const { rows } = await pool.query("SELECT * FROM endpoints WHERE user_id = $1", [userId]);
+
+        return res.status(200).json({ data: rows })
+
+    } catch (err) {
+        return res.status(500).json({ error: "Couldn't get endpoint." });
+    }
+}
