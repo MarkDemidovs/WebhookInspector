4c22486d9be1461a44ae23f4c092c799577b0d52
Mark Demidovs <markdemidovs@proton.me>
2026-03-16 23:25:10 +0200

    fix: smaller bugs

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,18 +1,43 @@
 import { useState } from "react";
+import { api } from "../lib/api";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const login = () => {
-        
+  // uh oh they deprecated formevent
+  const login = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    try {
+      const data = await api("/auth/login", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (data.message === "Logged in") {
+        navigate("/dashboard");
+      }
+    } catch (err) {
+        console.error(err);
     }
-    return ( 
-        <form onSubmit={login}>
-            <input type="email" value={email} onChange={(e => setEmail(e.target.value))}></input>
-            <input type="password" value={password} onChange={e => setPassword(e.target.value)}></input>
-            
-            <button>Send!</button>
-        </form>
-    )
+  };
+
+  return (
+    <form onSubmit={login}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      ></input>
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
+
+      <button>Send!</button>
+    </form>
+  );
 }
