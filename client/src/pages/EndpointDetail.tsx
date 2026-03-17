a934c95463a5d268c47883aa45be7bb7b44f840e
Mark Demidovs <markdemidovs@proton.me>
2026-03-17 23:52:53 +0200

    feat: Complete frontend endpointdetail

--- client/src/pages/EndpointDetail.tsx
+++ client/src/pages/EndpointDetail.tsx
@@ -2,24 +2,39 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { api } from "../lib/api";
 
+type Request = {
+  id: string;
+  method: string;
+  body: string;
+  headers: object;
+  ip: string;
+  received_at: string;
+  share_token: string;
+};
+
 export default function EndpointDetail() {
-    const { id } = useParams();
-    const [requests, setRequests] = useState([])
-    
-    useEffect(() => {
-        const fetchEndpoints = async () => {
-            try {
-                const data = await api(`/endpoints/${id}/requests`);
+  const { id } = useParams();
+  const [requests, setRequests] = useState<Request[]>([]);
 
-                setRequests(data.data);
-            } catch (err) {
-                console.error(err)
-            }
-        }
-        fetchEndpoints();
-    }, [])
+  useEffect(() => {
+    const fetchEndpoints = async () => {
+      try {
+        const data = await api(`/endpoints/${id}/requests`);
 
-    return(
-        
-    )
+        setRequests(data.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchEndpoints();
+  }, []);
+
+  return requests.map((request) => (
+    <div key={request.id}>
+      <p>{request.method}</p>
+      <p>{request.body}</p>
+      <p>{JSON.stringify(request.headers)}</p> <p>{request.received_at}</p>
+      <p>{request.share_token}</p>
+    </div>
+  ));
 }
